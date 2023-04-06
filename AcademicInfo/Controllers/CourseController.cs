7c956d396c3f5786d07898b0ae6100f9f7c4f3a3
sch990428 <990428sch@naver.com>
2023-04-06 09:59:24 +0900

    학사정보 Database 스키마 수정 및 수강과목 쿼리문 업데이트

--- AcademicInfo/Controllers/CourseController.cs
+++ AcademicInfo/Controllers/CourseController.cs
@@ -18,7 +18,7 @@ namespace AcademicInfo.Controllers
         public int Year { get; set; } // 개설 년도
 
         [JsonPropertyName("semester")]
-        public int Semester { get; set; } // 개설 학기
+        public string Semester { get; set; } // 개설 학기
 
         [JsonPropertyName("name")]
         public string Name { get; set; } // 교과목명
@@ -58,7 +58,7 @@ namespace AcademicInfo.Controllers
             var connectionString = "Host=\r\nacademic-info-db.postgres.database.azure.com\r\n;Username=classhub;Password=ch55361!;Database=AcademicInfo";
             var connection = new NpgsqlConnection(connectionString);
 
-            var query = "SELECT * FROM course";
+            var query = "SELECT\r\n    Section.section_id AS number,\r\n    Section.year,\r\n    Section.semester,\r\n    Course.title AS name,\r\n    Instructor.name AS instructor,\r\n    Time_slot.day_of_week AS day,\r\n    Time_slot.start_time AS startTime,\r\n    Time_slot.end_time AS endTime,\r\n    Section.room_id AS classRoom\r\nFROM\r\n    Takes\r\n    JOIN Section ON Takes.course_id = Section.course_id\r\n        AND Takes.section_id = Section.section_id\r\n        AND Takes.semester = Section.semester\r\n        AND Takes.year = Section.year\r\n    JOIN Course ON Takes.course_id = Course.course_id\r\n    JOIN Teaches ON Takes.course_id = Teaches.course_id\r\n        AND Takes.section_id = Teaches.section_id\r\n        AND Takes.semester = Teaches.semester\r\n        AND Takes.year = Teaches.year\r\n    JOIN Instructor ON Teaches.instructor_id = Instructor.ID\r\n    JOIN Time_slot ON Section.section_time_slot_id = Time_slot.section_time_slot_id\r\nWHERE\r\n    Takes.student_id = 3;";
             var results = connection.Query<CourseResponse>(query);
 
             connection.Dispose();
