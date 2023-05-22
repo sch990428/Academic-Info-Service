753ffe747d2d30de695032b556ee779ad98dfe82
sch990428 <990428sch@naver.com>
2023-05-22 16:51:55 +0900

    학생계정 및 교수계정 확인

--- AcademicInfo/Controllers/SectionController.cs
+++ AcademicInfo/Controllers/SectionController.cs
@@ -2,6 +2,8 @@ using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using System.Text.Json.Serialization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
 
 namespace AcademicInfo.Controllers
 {
@@ -45,21 +47,46 @@ namespace AcademicInfo.Controllers
         [HttpPost]
         public async Task<IActionResult> POST([FromQuery] int student_id)
         {
-            //받은 AccessToken을 인증하는 부분을 추가할 예정 
-
-            //Console.WriteLine("요청받음");
             var connectionString = "Host=\r\nacademic-info-db.postgres.database.azure.com\r\n;Username=classhub;Password=ch55361!;Database=AcademicInfo";
             var connection = new NpgsqlConnection(connectionString);
 
-            var query = "SELECT\r\n    Section.section_id AS sectionid,\r\n    Section.year,\r\n    Section.semester,\r\n    Course.title AS name,\r\n    Instructor.name AS instructor,\r\n    Time_slot.day_of_week AS day,\r\n    Time_slot.start_time AS startTime,\r\n    Time_slot.end_time AS endTime,\r\n    Section.room_id AS buildingid\r\nFROM\r\n    Takes\r\n    JOIN Section ON Takes.course_id = Section.course_id\r\n        AND Takes.section_id = Section.section_id\r\n        AND Takes.semester = Section.semester\r\n        AND Takes.year = Section.year\r\n    JOIN Course ON Takes.course_id = Course.course_id\r\n    JOIN Teaches ON Takes.course_id = Teaches.course_id\r\n        AND Takes.section_id = Teaches.section_id\r\n        AND Takes.semester = Teaches.semester\r\n        AND Takes.year = Teaches.year\r\n    JOIN Instructor ON Teaches.instructor_id = Instructor.ID\r\n    JOIN Time_slot ON Section.section_time_slot_id = Time_slot.section_time_slot_id\r\nWHERE\r\n    Takes.student_id = @student_id;";
+            //해당 id가 학생의 계정에 존재한다면
+            var student_exist_query = "SELECT COUNT(*) FROM student WHERE id = @id";
+            var professor_exist_query = "SELECT COUNT(*) FROM instructor WHERE id = @id";
             var parameters = new DynamicParameters();
-            parameters.Add("student_id", student_id);
+            parameters.Add("id", student_id);
 
-            var results = connection.Query<SectionResponse>(query, parameters);
+            if (connection.ExecuteScalar<int>(student_exist_query, parameters) == 1) {
 
-            connection.Dispose();
+                var query = "SELECT\r\n    Section.section_id AS sectionid,\r\n    Section.year,\r\n    Section.semester,\r\n    Course.title AS name,\r\n    Instructor.name AS instructor,\r\n    Time_slot.day_of_week AS day,\r\n    Time_slot.start_time AS startTime,\r\n    Time_slot.end_time AS endTime,\r\n    Section.room_id AS buildingid\r\nFROM\r\n    Takes\r\n    JOIN Section ON Takes.course_id = Section.course_id\r\n        AND Takes.section_id = Section.section_id\r\n        AND Takes.semester = Section.semester\r\n        AND Takes.year = Section.year\r\n    JOIN Course ON Takes.course_id = Course.course_id\r\n    JOIN Teaches ON Takes.course_id = Teaches.course_id\r\n        AND Takes.section_id = Teaches.section_id\r\n        AND Takes.semester = Teaches.semester\r\n        AND Takes.year = Teaches.year\r\n    JOIN Instructor ON Teaches.instructor_id = Instructor.ID\r\n    JOIN Time_slot ON Section.section_time_slot_id = Time_slot.section_time_slot_id\r\nWHERE\r\n    Takes.student_id = @student_id;";
+                var parameters2 = new DynamicParameters();
+                parameters2.Add("student_id", student_id);
 
-            return Ok(results);
+                var results = connection.Query<SectionResponse>(query, parameters2);
+
+                return Ok(results);
+
+            } else if (connection.ExecuteScalar<int>(professor_exist_query, parameters) == 1) {
+
+                //해당 id가 학생이 아니라면 교수를 검사
+                var query = "SELECT Section.section_id AS number, Section.year, Section.semester, Course.title AS name, Instructor.name AS instructor, Time_slot.day_of_week AS day, Time_slot.start_time AS startTime, Time_slot.end_time AS endTime, Section.room_id AS buildingid\r\nFROM teaches \r\nJOIN Section ON teaches.course_id = Section.course_id AND teaches.section_id = Section.section_id AND teaches.semester = Section.semester AND teaches.year = Section.year\r\nJOIN Course ON teaches.course_id = Course.course_id\r\nJOIN instructor ON teaches.instructor_id = instructor.id\r\nJOIN Time_slot ON Section.section_time_slot_id = Time_slot.section_time_slot_id\r\nWHERE teaches.instructor_id = @instructor_id";
+                var parameters2 = new DynamicParameters();
+                parameters2.Add("instructor_id", student_id);
+
+                var results = connection.Query<SectionResponse>(query, parameters2);
+
+                return Ok(results);
+
+            }
+
+            //일단은 반환하게 한다
+            var query3 = "SELECT\r\n    Section.section_id AS sectionid,\r\n    Section.year,\r\n    Section.semester,\r\n    Course.title AS name,\r\n    Instructor.name AS instructor,\r\n    Time_slot.day_of_week AS day,\r\n    Time_slot.start_time AS startTime,\r\n    Time_slot.end_time AS endTime,\r\n    Section.room_id AS buildingid\r\nFROM\r\n    Takes\r\n    JOIN Section ON Takes.course_id = Section.course_id\r\n        AND Takes.section_id = Section.section_id\r\n        AND Takes.semester = Section.semester\r\n        AND Takes.year = Section.year\r\n    JOIN Course ON Takes.course_id = Course.course_id\r\n    JOIN Teaches ON Takes.course_id = Teaches.course_id\r\n        AND Takes.section_id = Teaches.section_id\r\n        AND Takes.semester = Teaches.semester\r\n        AND Takes.year = Teaches.year\r\n    JOIN Instructor ON Teaches.instructor_id = Instructor.ID\r\n    JOIN Time_slot ON Section.section_time_slot_id = Time_slot.section_time_slot_id\r\nWHERE\r\n    Takes.student_id = @student_id;";
+            var parameters3 = new DynamicParameters();
+            parameters3.Add("student_id", student_id);
+
+            var results3 = connection.Query<SectionResponse>(query3, parameters3);
+
+            return Ok(results3);
         }
     };
 }
