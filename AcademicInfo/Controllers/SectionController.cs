42538ce2b3c31a38b00ab4aa2a92664803709d69
sch990428 <990428sch@naver.com>
2023-05-24 22:29:57 +0900

    course_id, section_id, semester, year에 해당하는 특정 강의실의 정보 제공

--- AcademicInfo/Controllers/SectionController.cs
+++ AcademicInfo/Controllers/SectionController.cs
@@ -4,15 +4,14 @@ using Npgsql;
 using System.Text.Json.Serialization;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Xml.Linq;
+using static AcademicInfo.Controllers.SectionResponse;
+using System.Reflection.Metadata;
 
-namespace AcademicInfo.Controllers
-{
-    public class SectionRequest
-    {
+namespace AcademicInfo.Controllers {
+    public class SectionRequest {
         private string AccessToken;
     }
-    public class SectionResponse
-    {
+    public class SectionResponse {
         public int SectionId { get; set; } // 강좌 번호
 
         public int Year { get; set; } // 개설 년도
@@ -30,6 +29,27 @@ namespace AcademicInfo.Controllers
         public int EndTime { get; set; } // 강의 종료시간
 
         public string BuildingId { get; set; } // 강의실
+    }
+    public class ClassRoom {
+        public int room_id { get; set; }
+        public string course_id { get; set; } // ABC12345
+        public int section_id { get; set; } // 0810
+        public string semester { get; set; } // SPRING, SUMMER, FALL, WINTER         
+        public int year { get; set; } // 2023
+        public string title { get; set; } // 캡스톤디자인
+    }
+
+    public class TimeSlot {
+        public DayOfWeek day_of_week { get; set; }
+        public int start_time { get; set; }
+        public int end_time { get; set; }
+    }
+
+    public class ClassRoomDetail : ClassRoom {
+        public List<TimeSlot> timeSlots { get; set; }
+        public string instructor { get; set; }
+        public string buildingId { get; set; }
+        public int color { get; set; } = -1;
     }
 
     [ApiController]
@@ -87,6 +107,43 @@ namespace AcademicInfo.Controllers
             var results3 = connection.Query<SectionResponse>(query3, parameters3);
 
             return Ok(results3);
+        }
+
+        [HttpGet("/ClassRoomDetail")]
+        public IActionResult ClassRoomDetail([FromQuery] string course_id, int section_id, string semester, int year, string accessToken) {
+            var connectionString = "Host=\r\nacademic-info-db.postgres.database.azure.com\r\n;Username=classhub;Password=ch55361!;Database=AcademicInfo";
+            var connection = new NpgsqlConnection(connectionString);
+
+            int semester_int = -1;
+            if (semester == "Spring") {
+                semester_int = 1;
+            } else if (semester == "Summer") {
+                semester_int = 2;
+            } else if (semester == "Fall") {
+                semester_int = 3;
+            } else if (semester == "Winter") {
+                semester_int = 4;
+            }
+
+            var query = "SELECT Instructor.name AS instructor, Section.room_id AS BuildingId\r\nFROM Section, Teaches, Instructor\r\nWHERE Teaches.section_id = Section.section_id AND Teaches.instructor_id = Instructor.id\r\nAND Section.course_id = @course_id AND Section.section_id = @section_id AND Section.semester = @semester AND Section.year = @year";
+            var parameters = new DynamicParameters();
+            parameters.Add("course_id", course_id);
+            parameters.Add("section_id", section_id);
+            parameters.Add("semester", semester_int);
+            parameters.Add("year", year);
+
+            var result = connection.QuerySingle<ClassRoomDetail>(query, parameters);
+
+            var query2 = "SELECT *\r\nFROM section, section_time_slot, time_slot\r\nWHERE section.section_time_slot_id = time_slot.section_time_slot_id AND time_slot.section_time_slot_id = section_time_slot.section_time_slot_id\r\nAND Section.course_id = @course_id AND Section.section_id = @section_id AND Section.semester = @semester AND Section.year = @year";
+            var time_slot_list = connection.Query<TimeSlot>(query2, parameters);
+
+            result.timeSlots = new List<TimeSlot>();
+
+            foreach (var slot in time_slot_list) {
+                result.timeSlots.Add(slot);
+            }
+
+            return Ok(result);
         }
     };
 }
